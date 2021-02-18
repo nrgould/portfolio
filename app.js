217d8b7a3297abdf68605252f3f8d3cfaf529c61
Nicholas Gould <nrgould197@gmail.com>
2021-02-18 18:13:35 -0500

    added project section

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 //for box animation
-const boxPath = document.querySelector("#box-path");
-const box1 = document.querySelector(".box1");
-const box2 = document.querySelector(".box2");
+const boxPath = document.querySelector('#box-path');
+const box1 = document.querySelector('.box1');
+const box2 = document.querySelector('.box2');
 
 //for Dark/Light Mode
-const darkBtn = document.querySelector(".appearance-toggle");
-const darkBtnIcon = document.querySelector(".appearance-toggle i");
+const darkBtn = document.querySelector('.appearance-toggle');
+const darkBtnIcon = document.querySelector('.appearance-toggle i');
 
 //for scrollmagic
-const animElement = document.querySelectorAll(".anim-el");
+const animElement = document.querySelectorAll('.anim-el');
 let controller;
 let pageScene;
 
 //for NAV
-const burger = document.querySelector(".burger");
-const navLinks = document.querySelector(".nav-links");
-const navOverlay = document.querySelector(".nav-overlay");
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+const navOverlay = document.querySelector('.nav-overlay');
 
 //for work section
-const workCard = document.querySelectorAll(".work-card");
-const readMoreBtn = document.querySelectorAll(".read-more");
+const workCard = document.querySelectorAll('.work-card');
+const readMoreBtn = document.querySelectorAll('.read-more');
 
 //for skills section
-const skillIcon = document.querySelectorAll(".icons-collection svg");
+const skillIcon = document.querySelectorAll('.icons-collection svg');
 
 //EVENT LISTENERS
-darkBtn.addEventListener("click", darkModeToggle);
-burger.addEventListener("click", navToggle);
-navLinks.addEventListener("click", closeNav);
+darkBtn.addEventListener('click', darkModeToggle);
+burger.addEventListener('click', navToggle);
+navLinks.addEventListener('click', closeNav);
 
 //BOX ANIMATION
 function boxAnim() {
@@ -37,7 +37,7 @@ function boxAnim() {
 		repeat: -1,
 		repeatDelay: 0,
 		yoyo: false,
-		ease: "power1.inOut(2)",
+		ease: 'power1.inOut(2)',
 		motionPath: {
 			path: boxPath,
 			align: boxPath,
@@ -51,7 +51,7 @@ function boxAnim() {
 		repeat: -1,
 		repeatDelay: 0,
 		yoyo: false,
-		ease: "power1.inOut(2)",
+		ease: 'power1.inOut(2)',
 		motionPath: {
 			path: boxPath,
 			align: boxPath,
@@ -69,28 +69,28 @@ function scrollReveal() {
 
 	animElement.forEach((el) => {
 		const pageTl = gsap.timeline({
-			defaults: { duration: 1, ease: "power2.inOut" },
+			defaults: { duration: 1, ease: 'power2.inOut' },
 		});
-		if (el.classList.contains("anim-right")) {
+		if (el.classList.contains('anim-right')) {
 			pageTl.fromTo(
 				el,
 				0.75,
-				{ x: "-20%", opacity: 0, scale: 0.8 },
-				{ x: "0%", opacity: 1, scale: 1 }
+				{ x: '-20%', opacity: 0, scale: 0.8 },
+				{ x: '0%', opacity: 1, scale: 1 }
 			);
-		} else if (el.classList.contains("anim-left")) {
+		} else if (el.classList.contains('anim-left')) {
 			pageTl.fromTo(
 				el,
 				0.75,
-				{ x: "20%", opacity: 0, scale: 0.8 },
-				{ x: "0%", opacity: 1, scale: 1 }
+				{ x: '20%', opacity: 0, scale: 0.8 },
+				{ x: '0%', opacity: 1, scale: 1 }
 			);
-		} else if (el.classList.contains("anim-up")) {
+		} else if (el.classList.contains('anim-up')) {
 			pageTl.fromTo(
 				el,
 				0.75,
-				{ y: "10%", opacity: 0 },
-				{ y: "0%", opacity: 1 }
+				{ y: '10%', opacity: 0 },
+				{ y: '0%', opacity: 1 }
 			);
 		}
 		pageScene = new ScrollMagic.Scene({
@@ -105,75 +105,60 @@ function scrollReveal() {
 
 //NAV TOGGLE
 function navToggle(e) {
-	if (!e.target.classList.contains("active")) {
-		e.target.classList.add("active");
-		navOverlay.style.display = "initial";
+	if (!e.target.classList.contains('active')) {
+		e.target.classList.add('active');
+		navOverlay.style.display = 'initial';
 		//burger => x
-		gsap.to(".line1", 0.5, {
-			ease: "power3.inOut",
-			rotate: "45",
+		gsap.to('.line1', 0.5, {
+			ease: 'power3.inOut',
+			rotate: '45',
 			y: 9,
 		});
-		gsap.to(".line2", 0.5, {
-			ease: "power3.inOut",
+		gsap.to('.line2', 0.5, {
+			ease: 'power3.inOut',
 			opacity: 0,
 		});
-		gsap.to(".line3", 0.5, {
-			ease: "power3.inOut",
-			rotate: "-45",
+		gsap.to('.line3', 0.5, {
+			ease: 'power3.inOut',
+			rotate: '-45',
 			y: -9,
 		});
-		//logo anim
-		gsap.fromTo(
-			"#logo",
-			0.35,
-			{ ease: "power2.inOut", scale: 0.55 },
-			{ ease: "power2.inOut", scale: 1.1 }
-		);
-		gsap
-			.fromTo(
-				"#logo",
-				0.5,
-				{ ease: "bounce.out", scale: 1.1 },
-				{ ease: "bounce.out", scale: 0.55 }
-			)
-			.delay(0.35);
 		//nav overlay open
 		gsap.fromTo(
 			navOverlay,
 			0.75,
 			{
 				opacity: 0,
-				ease: "power3.inOut",
-				x: "100%",
+				ease: 'power3.inOut',
+				x: '100%',
 			},
 			{
 				opacity: 1,
-				ease: "power3.inOut",
-				x: "0%",
+				ease: 'power3.inOut',
+				x: '0%',
 			}
 		);
 		// gsap.to(".nav-overlay", 1, {
 		// 	ease: "power3.inOut",
 		// 	clipPath: "circle(2500px at 100% -10%)",
 		// });
-		document.body.classList.add("hide");
+		document.body.classList.add('hide');
 	} else {
-		e.target.classList.remove("active");
+		e.target.classList.remove('active');
 		//x => burger
-		gsap.to(".line1", 0.5, {
-			ease: "power3.inOut",
-			rotate: "0",
+		gsap.to('.line1', 0.5, {
+			ease: 'power3.inOut',
+			rotate: '0',
 			y: 0,
 		});
-		gsap.to(".line2", 0.5, {
-			ease: "power3.inOut",
+		gsap.to('.line2', 0.5, {
+			ease: 'power3.inOut',
 			opacity: 1,
 			x: 0,
 		});
-		gsap.to(".line3", 0.5, {
-			ease: "power3.inOut",
-			rotate: "0",
+		gsap.to('.line3', 0.5, {
+			ease: 'power3.inOut',
+			rotate: '0',
 			y: 0,
 		});
 
@@ -183,58 +168,58 @@ function navToggle(e) {
 			0.75,
 			{
 				opacity: 1,
-				ease: "power3.inOut",
-				x: "0%",
+				ease: 'power3.inOut',
+				x: '0%',
 			},
 			{
 				opacity: 0,
-				ease: "power3.inOut",
-				x: "100%",
+				ease: 'power3.inOut',
+				x: '100%',
 			}
 		);
 		// gsap.to(".nav-overlay", 1, {
 		// 	ease: "power3.inOut",
 		// 	clipPath: "circle(50px at 100% -10%)",
 		// });
-		document.body.classList.remove("hide");
+		document.body.classList.remove('hide');
 	}
 }
 
 function closeNav() {
-	burger.classList.remove("active");
-	gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
-	gsap.to(".line2", 0.5, { opacity: 1, x: 0, background: "white" });
-	gsap.to(".line3", 0.5, { rotate: "0", y: 0, background: "white" });
+	burger.classList.remove('active');
+	gsap.to('.line1', 0.5, { rotate: '0', y: 0, background: 'white' });
+	gsap.to('.line2', 0.5, { opacity: 1, x: 0, background: 'white' });
+	gsap.to('.line3', 0.5, { rotate: '0', y: 0, background: 'white' });
 
 	gsap.fromTo(
 		navOverlay,
 		0.75,
 		{
 			opacity: 1,
-			ease: "power3.inOut",
-			x: "0%",
+			ease: 'power3.inOut',
+			x: '0%',
 		},
 		{
 			opacity: 0,
-			ease: "power3.inOut",
-			x: "100%",
+			ease: 'power3.inOut',
+			x: '100%',
 		}
 	);
 	// gsap.to(".nav-overlay", 1, { clipPath: "circle(50px at 100% -10%)" });
-	document.body.classList.remove("hide");
+	document.body.classList.remove('hide');
 }
 
 //DARK MODE
-const skillsListItem = document.querySelectorAll(".skills-list li");
-const skillsList = document.querySelectorAll(".skills-list");
-const skillIcon2 = document.querySelectorAll(".skill-icon");
+const skillsListItem = document.querySelectorAll('.skills-list li');
+const skillsList = document.querySelectorAll('.skills-list');
+const skillIcon2 = document.querySelectorAll('.skill-icon');
 
 function darkModeToggle() {
-	const darkItem = document.querySelectorAll(".dark-item");
+	const darkItem = document.querySelectorAll('.dark-item');
 	gsap.fromTo(
 		darkBtn,
 		0.25,
-		{ ease: "power3.inOut", scale: 0.5 },
+		{ ease: 'power3.inOut', scale: 0.5 },
 		{ scale: 1 }
 	);
 	// gsap.fromTo(
@@ -243,11 +228,11 @@ function darkModeToggle() {
 	// 	{ ease: "power3.inOut", rotate: 0 },
 	// 	{ ease: "power3.inOut", rotate: -360 }
 	// );
-	darkBtnIcon.classList.toggle("fa-sun");
-	darkBtnIcon.classList.toggle("fa-moon");
-	document.body.classList.toggle("light");
+	darkBtnIcon.classList.toggle('fa-sun');
+	darkBtnIcon.classList.toggle('fa-moon');
+	document.body.classList.toggle('light');
 	darkItem.forEach((item) => {
-		item.classList.toggle("light");
+		item.classList.toggle('light');
 	});
 }
 
